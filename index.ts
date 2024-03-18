31da2728547b07639419f9c16a7c908f671d3b5e
aligeventt <ali.geven@tooltime.de>
2024-03-18 13:26:06 +0100

    add bade code

--- index.ts
+++ index.ts
@@ -4,11 +4,11 @@ function f<T>(a1: T, a2: T, a3: T): boolean {
     return a2 <= a1 && a1 <= a3;
 }
 
-export function hello(who: string = world): string {
+export function x(who: string = world): string {
     return `Hello ${who}! `;
 }
 
 
-console.log('Hello world!' === hello());
-console.log('Hello world!' === hello('world'));
+console.log('Hello world!' === x());
+console.log('Hello world!' === x('world'));
 console.log(f(1, 2, 3))
