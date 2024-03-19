f49deb70eaa2fbd4e2ca5791a922f217230cbe6f
Ali Onur Geven <141142027+aligeventt@users.noreply.github.com>
2024-03-19 14:09:49 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,16 +1,18 @@
-const a = 'x';
-const b = 'y';
-const c = 'z';
+class Animal {
+  public name;
+  constructor(n) {
+    this.name = n;
+  }
 
-function f<T>(a1: T, a2: T, a3: T): boolean {
-    return a2 <= a1 && a1 <= a3;
+  bark() {
+    console.log('The ' + this.name + ' is barking.');
+  }
+
+  eat(food) {
+    console.log('The ' + this.name + ' is eating ' + food + '.');
+  }
 }
 
-export function x(t: string = a): string {
-    return `Hello ${t}! `;
-}
-
-
-console.log('Hello world!' === x());
-console.log('Hello world!' === x('world'));
-console.log(f(1, 2, 3))
+let dog = new Animal('Dog');
+dog.bark();
+dog.eat('bone');
